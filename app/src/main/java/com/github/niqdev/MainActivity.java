75c1545cd42c833fed518de86ea9443f68bf58bc
niqdev <niqdev@gmail.com>
2015-12-18 18:50:07 +0100

    setup demo: change text

--- app/src/main/java/com/github/niqdev/MainActivity.java
+++ app/src/main/java/com/github/niqdev/MainActivity.java
@@ -1,13 +1,33 @@
 package com.github.niqdev;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.BindString;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindString(R.string.text_after)
+    String textAfter;
+
+    @Bind(R.id.myTextView)
+    TextView myTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
     }
+
+    @OnClick(R.id.myButton)
+    void onClickChangeText() {
+        myTextView.setText(textAfter);
+    }
+
 }
